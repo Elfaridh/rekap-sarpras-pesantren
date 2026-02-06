56f386d2015848ff690d0c5ef5fd5e5889cff4d1
Elfaridh <92539545+Elfaridh@users.noreply.github.com>
2026-02-06 09:05:00 +0700

    Create lokasi.js

---
+++ js/lokasi.js
@@ -0,0 +1,4 @@
+function tambahLokasi(nama) {
+  const tx = db.transaction("lokasi", "readwrite");
+  tx.objectStore("lokasi").add({ Gedung Asrama Putra, Gedung Asrama Putri, Kantor TU, Kantor Manajemen, Kelas XA });
+}
